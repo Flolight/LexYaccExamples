031c9502be582d2ea8b35d06d2a9e6150b4ae6d5
flolight <florianclanet@gmail.com>
2017-02-02 00:25:18 +0100

    punctuation spaces

--- simple_ones/cleanup/clean.c
+++ simple_ones/cleanup/clean.c
@@ -368,8 +368,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -377,9 +377,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[11] =
+static yyconst flex_int16_t yy_accept[14] =
     {   0,
-        0,    0,    4,    3,    2,    3,    2,    0,    1,    0
+        0,    0,    5,    4,    2,    4,    4,    2,    0,    0,
+        1,    3,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
@@ -390,14 +391,14 @@ static yyconst flex_int32_t yy_ec[256] =
         1,    2,    3,    1,    1,    1,    1,    1,    1,    1,
         4,    1,    1,    3,    1,    3,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    3,    3,    1,
-        1,    1,    3,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    3,    1,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        1,    1,    1,    1,    1,    1,    5,    5,    5,    5,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -414,33 +415,33 @@ static yyconst flex_int32_t yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst flex_int32_t yy_meta[5] =
+static yyconst flex_int32_t yy_meta[6] =
     {   0,
-        1,    2,    1,    1
+        1,    2,    1,    1,    1
     } ;
 
-static yyconst flex_int16_t yy_base[13] =
+static yyconst flex_int16_t yy_base[17] =
     {   0,
-        0,    0,    8,    9,    0,    0,    0,    4,    9,    9,
-        4,    3
+        0,    0,   11,   12,    0,    0,    0,    0,    7,    6,
+       12,   12,   12,    6,    5,    4
     } ;
 
-static yyconst flex_int16_t yy_def[13] =
+static yyconst flex_int16_t yy_def[17] =
     {   0,
-       10,    1,   10,   10,   11,   12,   11,   12,   10,    0,
-       10,   10
+       13,    1,   13,   13,   14,   15,   16,   14,   15,   16,
+       13,   13,    0,   13,   13,   13
     } ;
 
-static yyconst flex_int16_t yy_nxt[14] =
+static yyconst flex_int16_t yy_nxt[18] =
     {   0,
-        4,    5,    4,    6,    8,    7,    9,   10,    3,   10,
-       10,   10,   10
+        4,    5,    4,    6,    7,   10,    9,    8,   12,   11,
+       13,    3,   13,   13,   13,   13,   13
     } ;
 
-static yyconst flex_int16_t yy_chk[14] =
+static yyconst flex_int16_t yy_chk[18] =
     {   0,
-        1,    1,    1,    1,   12,   11,    8,    3,   10,   10,
-       10,   10,   10
+        1,    1,    1,    1,    1,   16,   15,   14,   10,    9,
+        3,   13,   13,   13,   13,   13,   13
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -460,7 +461,7 @@ char *yytext;
 #line 1 "clean.l"
 #line 2 "clean.l"
 
-#line 464 "<stdout>"
+#line 465 "<stdout>"
 
 #define INITIAL 0
 
@@ -650,7 +651,7 @@ YY_DECL
 #line 7 "clean.l"
 
 
-#line 654 "<stdout>"
+#line 655 "<stdout>"
 
 	if ( !(yy_init) )
 		{
@@ -703,13 +704,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 14 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 9 );
+		while ( yy_base[yy_current_state] != 12 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -744,14 +745,22 @@ YY_RULE_SETUP
 case 2:
 YY_RULE_SETUP
 #line 10 "clean.l"
-{ printf(" "); }
+{ printf(" "); } /* Remove multiple spaces */
 	YY_BREAK
 case 3:
+*yy_cp = (yy_hold_char); /* undo effects of setting up yytext */
+(yy_c_buf_p) = yy_cp -= 1;
+YY_DO_BEFORE_ACTION; /* set up yytext again */
 YY_RULE_SETUP
-#line 12 "clean.l"
+#line 11 "clean.l"
+{ while (yytext[yyleng-1]==' ') yyleng--; ECHO; } /* Remove space before punctuation */
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 13 "clean.l"
 ECHO;
 	YY_BREAK
-#line 755 "<stdout>"
+#line 764 "<stdout>"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1043,7 +1052,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 14 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1071,11 +1080,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 14 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 13);
 
 	return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1749,7 +1758,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "clean.l"
+#line 13 "clean.l"
 
 
 
